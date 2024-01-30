f37f8f82b299037d64f4a2c66387564c8aa2d004
gminiy <gmini.y@gmail.com>
2024-01-30 22:13:33 +0900

    feat: chargerInfo styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,32 @@
+import { createGlobalStyle } from "styled-components";
+import ChargerInfo from "./components/chargerInfo";
+const GlobalStyle = createGlobalStyle`
+  body {
+    background-color: #f5f5f5; // 옅은 회색 배경
+  }
+`;
+
+const mockCharger = {
+  id: "20",
+  chargeType: 2,
+  location: "B 구역 3",
+  status: 1,
+  lastStatusUpdatedAt: 1706156202,
+  output: 7,
+  lastStartChargingTimestamp: 1706155870,
+  lastEndChargingTimestamp: 1706159470,
+};
+
 function App() {
-  return <div></div>;
+  return (
+    <>
+      <GlobalStyle />
+      <ChargerInfo
+        charger={mockCharger}
+        address="서울특별시 강남구 역삼동 1000-100"
+      />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/chargerInfo.tsx
@@ -0,0 +1,179 @@
+import styled from "styled-components";
+
+type Charger = {
+  id: string;
+  chargeType: number;
+  location: string;
+  status: number;
+  lastStatusUpdatedAt: number;
+  output: number;
+  lastStartChargingTimestamp?: number;
+  lastEndChargingTimestamp?: number;
+};
+
+type ChargerInfoProps = {
+  charger: Charger;
+  address: string;
+};
+
+type StatusProps = {
+  isAvailableCharger: boolean;
+};
+
+const CHARGER_TYPES = [
+  "DC 차데모",
+  "AC 완속",
+  "DC차데모+AC3상",
+  "DC콤보",
+  "DC차데모+DC콤보",
+  "DC차데모+AC3상",
+  "+DC콤보",
+  "AC3상",
+];
+
+const STATUS = [
+  "통신이상",
+  "충전가능",
+  "충전중",
+  "운영중지",
+  "점검중",
+  "상태미확인",
+];
+
+const ChargerInfoBlock = styled.div`
+  background-color: #ffffff; // 옅은 회색 배경
+  padding: 16px 32px;
+  line-height: 1.5;
+`;
+
+const Title = styled.div`
+  font-size: 24px;
+  font-weight: bold;
+  color: #333;
+  margin-bottom: 8px;
+  line-height: 1.5;
+`;
+
+const Location = styled.div`
+  font-size: 20px;
+  font-weight: bold;
+  color: #555555;
+  line-height: 1.5;
+`;
+
+const Address = styled.div`
+  font-size: 16px;
+  color: #666;
+  margin-bottom: 24px;
+`;
+
+const Output = styled.div`
+  font-size: 20px;
+  font-weight: bold;
+  color: #555555;
+  margin-bottom: 8px;
+  margin-right: 16px;
+`;
+
+const ChargerType = styled.div`
+  font-size: 14px;
+  color: #666;
+  margin-bottom: 8px;
+  padding: 0px 10px;
+  border-left: 3px solid #eee;
+`;
+
+const Card = styled.div`
+  background-color: #f5f5f5; // 연한 회색 배경
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1); // 그림자 효과
+  border-radius: 8px; // 둥근 모서리
+  padding: 20px; // 내부 여백
+  margin: 10px 0; // 위아래 여백
+  max-width: 300px; // 최대 너비
+  box-sizing: border-box; // 박스 크기 계산 방식
+`;
+
+const Status = styled.div<StatusProps>`
+  font-size: 20px;
+  font-weight: bold;
+  color: ${(props) =>
+    props.isAvailableCharger ? "#28a745" : "#dc3545"}; // 조건부 색상
+  margin-bottom: 8px;
+`;
+
+const ChargingTimeInfo = styled.div`
+  font-size: 16px;
+  color: #555;
+  margin-bottom: 8px;
+  padding-left: 10px;
+  border-left: 2px solid #ddd;
+  line-height: 1.4;
+`;
+
+const OutputBlock = styled.div`
+  display: flex;
+  justify-content: start;
+  align-items: center;
+`;
+
+const getTimeDifferenceString = (timestamp: number) => {
+  if (!timestamp) return "";
+
+  const now = Date.now();
+  const differenceInSeconds = Math.floor((now - timestamp * 1000) / 1000);
+  const minutes = Math.floor(differenceInSeconds / 60);
+  const hours = Math.floor(minutes / 60);
+  const days = Math.floor(hours / 24);
+
+  if (days > 0) {
+    return `${days}일 전`;
+  } else if (hours > 0) {
+    return `${hours}시간 ${minutes % 60}분 전`;
+  } else {
+    return `${minutes}분 전`;
+  }
+};
+
+const ChargerInfo: React.FC<ChargerInfoProps> = ({ address, charger }) => {
+  let chargingTimeInfoString;
+
+  if (charger.lastStartChargingTimestamp && charger.lastEndChargingTimestamp) {
+    if (charger.lastStartChargingTimestamp > charger.lastEndChargingTimestamp) {
+      chargingTimeInfoString = `${getTimeDifferenceString(
+        charger.lastStartChargingTimestamp
+      )} 시작`;
+    } else {
+      chargingTimeInfoString = `${getTimeDifferenceString(
+        charger.lastEndChargingTimestamp
+      )} 종료`;
+    }
+  } else if (
+    charger.lastStartChargingTimestamp &&
+    !charger.lastEndChargingTimestamp
+  ) {
+    chargingTimeInfoString = `${getTimeDifferenceString(
+      charger.lastStartChargingTimestamp
+    )} 시작`;
+  }
+
+  return (
+    <ChargerInfoBlock>
+      <Title>충전기 정보</Title>
+      <Location>{charger.location}</Location>
+      <Address>{address}</Address>
+      <OutputBlock>
+        <Output>{charger.output}kW</Output>
+        <ChargerType>{CHARGER_TYPES[charger.chargeType]}</ChargerType>
+      </OutputBlock>
+
+      <Card>
+        <Status isAvailableCharger={charger.status === 1}>
+          {STATUS[charger.status]}
+        </Status>
+        <ChargingTimeInfo>{chargingTimeInfoString}</ChargingTimeInfo>
+      </Card>
+    </ChargerInfoBlock>
+  );
+};
+
+export default ChargerInfo;
